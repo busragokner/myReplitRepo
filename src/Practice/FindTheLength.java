268b50d6d3118090bb48846ba8a92f0df708b710
OmerOzdil <omerozdil54@gmail.com>
2022-06-13 00:30:46 +0100

    new code has been added

---
+++ src/Practice/FindTheLength.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class FindTheLength {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Please enter the text:")
+        String text=scan.next();
+        int length=text.length();
+    }
+}
--- src/Practice/RealEstateCal.java
+++ src/Practice/RealEstateCal.java
@@ -4,50 +4,101 @@ import java.util.Scanner;
 
 public class RealEstateCal {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int propertyPrice = 0;
-        int numberOfBedrooms, garageSpots;
-        float metroAccessibility, schoolScore, highwayAccessibility;
-        boolean backyard, smoking, garage;
-        //this is the line I've added to control remote repo
+        int numberOfBedrooms;
+
+
+
 
         System.out.println("*****************************************");
         System.out.println("* Welcome to the RealEstate calculator! *");
         System.out.println("*****************************************");
 
         System.out.println("Enter your property type:");
-        String houseType=scan.next();
-        if(houseType.equals("Condo")){
-            propertyPrice+=50000;
-        }else if (houseType.equals("Townhouse")){
-            propertyPrice+=75000;
-        }else if (houseType.equals("Single Family Home")){
-            propertyPrice+=95000;
+        String houseType = scan.nextLine();
+        if (houseType.equals("Condo")) {
+            propertyPrice += 50000;
+        } else if (houseType.equals("Townhouse")) {
+            propertyPrice += 75000;
+        } else if (houseType.equals("Single Family Home")) {
+            propertyPrice += 95000;
         }
+
+
         System.out.println("How many bedrooms do you have?");
-        int howmany=1;
-        int oneBed=30000;
-        propertyPrice+=30000*howmany;
-        System.out.println(propertyPrice);
+        numberOfBedrooms = scan.nextInt();
+        propertyPrice += 30000 * numberOfBedrooms;
 
         System.out.println("Do you have a backyard?");
-        backyard=true;
-        if(backyard==true){
-            propertyPrice+= 5000;
-            if(houseType.equals("Condo")){
-                System.out.println("Backyard is not available for condo!");
+        boolean backyard = scan.nextBoolean();
+
+        if (houseType.equals("Condo")) {
+            System.out.println("Backyard is not available for condo!");
+        }else{
+            if (backyard == true) {
+                propertyPrice += 5000;
             }
 
         }
 
+        System.out.println("Do you have garage?");
+        boolean garage = scan.nextBoolean();
+        if(garage==true) {
+            System.out.println("How many spots?");
+            int howManySpot = scan.nextInt();
+            if (howManySpot > 10) {
+                System.out.println("Pardon, it's not public parking!");
+            }else{
+                propertyPrice += howManySpot * 20000;
+            }
 
-        /*
+            }
 
-         */
+        System.out.println("How close is metro station?");
+        float metroAccessibility = scan.nextFloat();
+        if(metroAccessibility<=1){
+            propertyPrice += 10000;
+    }else if(metroAccessibility>1 && metroAccessibility<3){
+
+            propertyPrice += 5000;}
+
+
+
+        System.out.println("How close is highway?");
+        float highwayAccessibility=scan.nextFloat();
+        if (highwayAccessibility<=1){
+            propertyPrice+=15000;
+        }else if (highwayAccessibility>1 && highwayAccessibility<5){
+            propertyPrice+=8000;
+        }else if (highwayAccessibility>=5 && highwayAccessibility<=20){
+            propertyPrice+=4000;
+        }
+
+        System.out.println("What's the rating of nearest school?");
+        float schoolScore=scan.nextFloat();
+        if (schoolScore<=10 && schoolScore>8){
+            propertyPrice+=45000;
+        }else if (schoolScore<8 && schoolScore>=4){
+            propertyPrice+=20000;
+        }else {
+            propertyPrice+=5000;
+        }
+
+        System.out.println("Does any of your family members smoke?");
+        boolean smoking=scan.nextBoolean();
+
+        if (smoking==true){
+            propertyPrice-=5000;
+        }
+        System.out.println("Market report has been generated.\nYour estimate market price is: "+propertyPrice+"$");
 
 
 
 
 
-    }
-}
+
+}}
+
+
+
