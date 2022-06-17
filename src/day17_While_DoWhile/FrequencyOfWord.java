d1ab825fb4403a035d19588767e90efea7cd0f3f
OmerOzdil <omerozdil54@gmail.com>
2022-06-17 09:50:18 +0100

    add my codes

--- src/day17_While_DoWhile/FrequencyOfWord.java
+++ src/day17_While_DoWhile/FrequencyOfWord.java
@@ -14,6 +14,19 @@ public class FrequencyOfWord {
             }
         }
         System.out.println(frequency);
+        /*
+        sentence="JavaJavaJava"
+        output=3
+
+        Str = "JavaJava";
+
+        substring:
+        1. Java // substring(0, 4)
+        2. avaJ // substring(1, 5)
+        3. vaJa // substring(2, 2+4)
+        4.aJav // substring(3, 3+4)
+        5.Java // substring(4, 4+4)
+         */
 
 
 
---
+++ src/day17_While_DoWhile/WhileLoopIntro.java
@@ -0,0 +1,18 @@
+package day17_While_DoWhile;
+
+public class WhileLoopIntro {
+    public static void main(String[] args) {
+
+        if (true){
+            System.out.println("Wooden Spoon");
+        }
+        String ayrac="-----------------------------------------------------------------";
+        System.out.println(ayrac);
+
+        while (true){
+            System.out.println("Wooden Spoon");
+        }
+
+
+    }
+}
