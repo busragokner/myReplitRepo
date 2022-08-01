e6519eb6f1701a4daa2700dfc60bc11bb9e7608f
OmerOzdil <omerozdil54@gmail.com>
2022-08-01 22:40:09 +0100

    adds new update

--- src/Practice/practice1.java
+++ src/Practice/practice1.java
@@ -1,19 +1,11 @@
 package Practice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class practice1 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 5, 6};
 
-        boolean has55 = false;
-
-        for (int i = 0; i < nums.length-1; i++) {
-
-            if (nums[i] ==5 && nums[i+1] ==5){
-                has55 = true;
-            }
-            System.out.println(has55);
 
 
         }
@@ -22,6 +14,26 @@ public class practice1 {
 
 
 
-                }
 
-            }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
